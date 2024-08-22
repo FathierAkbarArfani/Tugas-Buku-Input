4379f1f08ec4786d2e2e172e612692972a87f675
FathierAkbarArfani <fathier_arfani_33rpl@student.smktelkom-mlg.sch.id>
2024-08-22 11:02:38 +0700

    Add files via upload

---
+++ BukuTester.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+public class BukuTester {
+   public static void main(String[] args) {
+
+Scanner in = new Scanner(System.in);
+
+System.out.println("Masukan Nama Buku");
+String judul = in.nextLine();
+System.out.println("Masukan Nama Pengarang Buku :");
+String pengarang = in.nextLine();
+System.out.println("Masukan ID :");
+double id = in.nextDouble();
+System.out.println("Masukan Tahun Terbit");
+double tahunterbit = in.nextDouble();
+
+ 
+System.out.println("===========================================");
+System.out.println("Judul : "+judul);
+System.out.println("Pengarang :"+pengarang);
+System.out.println("ID :"+id);
+System.out.println("Tahun Terbit :"+tahunterbit);
+System.out.println("===========================================");
+
+
+   
+      // Buku skibidi = new Buku("Skibidi", 177013.00, "Ligma", 2024.00);
+      // Buku juki = new Buku("JukiSigma", 24414, "JukiHoki", 1945);
+      // Buku nemoto = new Buku("nemoto", 3, "Aqua", 2021);
+      // Buku ligma = new Buku("Ligma", 200120, "Sapwan", 2024);
+      // Buku mewing = new Buku("Mewing", 212234, "Sapwan", 2024);
+
+
+//       System.out.print("buku 1:"); { 
+
+//       System.out.print("Nama Buku:"+skibidi.judul+" ");
+//       System.out.print("ID Buku:"+skibidi.id+" ");
+//       System.out.print("Pengarang Buku"+skibidi.pengarang+" ");
+//       System.out.print("tahun terbit:"+skibidi.tahunterbit+" "); };
+// System.out.println(" ");
+// System.out.println(" ");
+//       System.out.print("buku 2:");
+
+//       System.out.print("Nama Buku:"+juki.judul+" ");
+//       System.out.print("Id Buku:"+juki.id+" ");
+//       System.out.print("Pengarang Buku:"+juki.pengarang+" ");
+//       System.out.print("tahun terbit:"+juki.tahunterbit+" ");
+// System.out.println(" ");  
+// System.out.println(" ");    
+
+//       System.out.print("buku 3:");
+
+//       System.out.print("judul buku:"+nemoto.judul+" ");
+//       System.out.print("ID Buku:"+nemoto.id+" ");
+//       System.out.print(nemoto.pengarang+" ");
+//       System.out.print(nemoto.tahunterbit+" ");
+// System.out.println(" ");  
+// System.out.println(" ");    
+
+//       System.out.print("buku 4:");
+
+//       System.out.print(ligma.judul+" ");
+//       System.out.print(ligma.id+" ");
+//       System.out.print(ligma.pengarang+" ");
+//       System.out.print(ligma.tahunterbit+" ");
+// System.out.println(" ");    
+// System.out.println(" ");  
+
+//       System.out.print("buku 5:");
+      
+//       System.out.print(mewing.judul+" ");
+//       System.out.print(mewing.id+" ");
+//       System.out.print(mewing.pengarang+" ");
+//       System.out.print(mewing.tahunterbit+" ");
+   }
+}
